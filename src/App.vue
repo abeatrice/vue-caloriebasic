316350eb41c7e74f672eec3fe17d9c9c081d1da5
abeatrice <abeatrice.mail@gmail.com>
2019-09-02 12:04:50 -0700

    login redirect and style

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
   <div id="app">
-    <NavTop />
-    <router-view></router-view>
+    <div class="bg-teal-100 h-screen w-full">
+      <NavTop />
+      <div class="container h-full pt-6 px4 mx-auto">
+        <router-view></router-view>
+      </div>
+    </div>
   </div>
 </template>
 
