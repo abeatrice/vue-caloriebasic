79e10d002239caf453d38a0badbc1b15aab793ec
abeatrice <abeatrice.mail@gmail.com>
2019-09-17 22:59:34 -0700

    adjust home calories position

--- src/components/body/Home.vue
+++ src/components/body/Home.vue
@@ -1,23 +1,23 @@
 <template>
   <div class="h-full flex justify-center items-center">
     <div class="w-3/4 md:w-1/2">    
-      <div class="flex justify-center align-center">
+      <p class="text-6xl text-center font-semibold">
+        {{calories.quantity}}
+      </p>
+      <div class="flex justify-center align-center my-5">
         <datepicker 
           @selected="updateSelectedDate"
           :value="selectedDate"
           :inline="true"
         />
       </div>
-      <p class="text-center text-xl font-semibold my-5">
-        {{calories.quantity}}
-      </p>
       <div class="flex justify-center align-center mb-2">
         <div @click="adjustCalories(10)" class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 mx-1 rounded focus:outline-none focus:shadow-outline cursor-pointer">10</div>
         <div @click="adjustCalories(25)" class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 mx-1 rounded focus:outline-none focus:shadow-outline cursor-pointer">25</div>
         <div @click="adjustCalories(100)" class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 mx-1 rounded focus:outline-none focus:shadow-outline cursor-pointer">100</div>
         <div @click="adjustCalories(250)" class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 mx-1 rounded focus:outline-none focus:shadow-outline cursor-pointer">250</div>
       </div>
-      <div class="flex justify-center align-center mb-2">
+      <div class="flex justify-center align-center">
         <div @click="adjustCalories(-10)" class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 mx-1 rounded focus:outline-none focus:shadow-outline cursor-pointer">10</div>
         <div @click="adjustCalories(-25)" class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 mx-1 rounded focus:outline-none focus:shadow-outline cursor-pointer">25</div>
         <div @click="adjustCalories(-100)" class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 mx-1 rounded focus:outline-none focus:shadow-outline cursor-pointer">100</div>
