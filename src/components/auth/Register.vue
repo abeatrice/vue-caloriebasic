579e7c0c8b009c05bdda434758b0a55991c175eb
abeatrice <abeatrice.mail@gmail.com>
2019-09-30 19:53:00 -0700

    form error feedback

--- src/components/auth/Register.vue
+++ src/components/auth/Register.vue
@@ -9,39 +9,41 @@
                     <label for="email" class="block text-teal-900 text-sm font-bold mb-2">User Name</label>
                     <input 
                         v-model="form.userName"
-                        @keyup="clearError"
-                        :class="{'border-red-500':hasError}"
                         :disabled="loading"
+                        @keyup="clearError('userName')"
+                        :class="{ 'border-red-500': hasError('userName') }"
                         class="shadow appearance-none border rounded w-full py-2 px-3 text-teal-900 leading-tight focus:outline-none focus:shadow-outline"
                         id="userName"
                         ref="userName" 
-                        type="userName" 
+                        type="text" 
                         placeholder="calorieStar23" 
                         required
                     >
+                    <p v-if="hasError('userName')" class="text-red-500 text-xs italic mt-1">{{errors.userName}}</p>
                 </div>
                 <div class="mb-4">
                     <label for="email" class="block text-teal-900 text-sm font-bold mb-2">Email Address</label>
                     <input 
                         v-model="form.email" 
-                        @keyup="clearError"
-                        :class="{'border-red-500':hasError}"
                         :disabled="loading"
+                        @keyup="clearError('email')"
+                        :class="{ 'border-red-500': hasError('email') }"
                         class="shadow appearance-none border rounded w-full py-2 px-3 text-teal-900 leading-tight focus:outline-none focus:shadow-outline"
                         id="email"
-                        ref="email" 
+                        ref="email"
                         type="email" 
-                        placeholder="janeDoe@example.com" 
+                        placeholder="janeDoe@example.com"
                         required
                     >
+                    <p v-if="hasError('email')" class="text-red-500 text-xs italic mt-1">{{errors.email}}</p>
                 </div>
                 <div class="mb-4">
                     <label for="password" class="block text-teal-900 text-sm font-bold mb-2">Password</label>
                     <input 
                         v-model="form.password"
-                        @keyup="clearError"
-                        :class="{'border-red-500':hasError}"
                         :disabled="loading"
+                        @keyup="clearError('password')"
+                        :class="{ 'border-red-500': hasError('password') }"
                         class="shadow appearance-none border rounded w-full py-2 px-3 text-teal-900 leading-tight focus:outline-none focus:shadow-outline" 
                         id="password"
                         ref="password"
@@ -51,21 +53,21 @@
                     >
                 </div>
                 <div class="mb-4">
-                    <label for="password-confirm" class="block text-teal-900 text-sm font-bold mb-2">Confirm Password</label>
+                    <label for="passwordConfirm" class="block text-teal-900 text-sm font-bold mb-2">Confirm Password</label>
                     <input 
                         v-model="form.passwordConfirm"
-                        @keyup="clearError"
-                        :class="{'border-red-500':hasError}"
                         :disabled="loading"
+                        @keyup="clearError('passwordConfirm')"
+                        :class="{ 'border-red-500': hasError('passwordConfirm') }"
                         class="shadow appearance-none border rounded w-full py-2 px-3 text-teal-900 leading-tight focus:outline-none focus:shadow-outline" 
-                        id="password-confirm"
-                        ref="password-confirm"
-                        type="password" 
+                        id="passwordConfirm"
+                        ref="passwordConfirm"
+                        type="password"
                         placeholder="*******" 
                         required
                     >
+                    <p v-if="hasError('passwordConfirm')" class="text-red-500 text-xs italic mt-1">{{errors.passwordConfirm}}</p>
                 </div>
-                <p v-if="hasError" class="text-red-500 text-xs italic mb-4">{{error}}</p>
                 <button
                     :disabled="loading"
                     type="submit" 
@@ -84,7 +86,12 @@ export default {
     data() {
         return {
             loading: false,
-            error: '',
+            errors: {
+                userName: '',
+                email: '',
+                password: '',
+                passwordConfirm: ''
+            },
             form: {
                 userName: '',
                 email: '',
@@ -94,9 +101,6 @@ export default {
         }
     },
     computed: {
-        hasError() {
-            return this.error !== '';
-        },
         year() {
             return new Date().getFullYear();
         }
@@ -105,12 +109,15 @@ export default {
         this.$refs.userName.focus();
     },
     methods: {
-        clearError() {
-            this.error = '';
+        hasError(field) {
+            return this.errors[field] !== '';
+        },
+        clearError(field) {
+            this.errors[field] = '';
         },
         onSubmit() {
             this.loading = true;
-            if(this.validateForm()) {
+            if(this.formIsValid()) {
                 this.$store.dispatch('register', this.form)
                 .then(() => {
                     this.loading = false;
@@ -118,36 +125,53 @@ export default {
                 })
                 .catch(error => {
                     this.loading = false;
-                    this.error = error;
+                    this.loadErrors(error.response.data);
                     this.form.password = '';
                     this.form.passwordConfirm = '';
                     this.$refs.password.focus();
                 });
             }
         },
-        validateForm() {
+        loadErrors(errors) {
+            if(errors.hasOwnProperty("errmsg") && errors.errmsg.includes("duplicate")) {
+                if(errors.errmsg.includes("userName")) {
+                    this.errors.userName = "This user name is taken!";
+                }
+                if(errors.errmsg.includes("email")) {
+                    this.errors.email = "This email address is taken!";
+                }
+            }
+            if(errors.errors.email !== undefined) { 
+                this.errors.email = "This is not a valid email!";
+            }
+        },
+        formIsValid() {
             for(let input of Object.entries(this.form)) {
                 if(input[1].trim() === '') {
-                    this.error = 'All Inputs are required!';
+                    this.errors[input[0]] = 'All fields are required!';
+                }
+
+                if(input[0] == 'userName' && input[1].trim().includes(' ')) {
+                    this.errors.userName = 'The user name cannot have a space!';
+                }
+
+            }
+
+            if (this.form.password !== this.form.passwordConfirm) {
+                this.errors.passwordConfirm = 'The passwords do not match!';
+            }
+
+            for(let error of Object.entries(this.errors)) {
+                console.log(error);
+                if(error[1] !== '') {
+                    this.form.password = '';
+                    this.form.passwordConfirm = '';
                     this.loading = false;
                     this.$refs.userName.focus();
                     return false;
                 }
             }
-            if(this.form.userName.trim().includes(' ')) {
-                this.error = 'The user name cannot have a space!';
-                this.loading = false;
-                this.$refs.userName.focus();
-                return false;
-            }
-            if (this.form.password !== this.form.passwordConfirm) {
-                this.error = 'The passwords do not match!';
-                this.loading = false;
-                this.form.password = '';
-                this.form.passwordConfirm = '';
-                this.$refs.password.focus();
-                return false;
-            }
+
             return true;
         }
     }
