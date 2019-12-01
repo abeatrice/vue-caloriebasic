a2efe5e91a7151f8c286fef7e2c8c7a03b8c49ce
abeatrice <abeatrice.mail@gmail.com>
2019-12-01 12:06:02 -0800

    Login Dark

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
-    <router-view></router-view>
+    <transition name="fade" mode="out-in">
+      <router-view></router-view>
+    </transition>
   </div>
 </template>
 
@@ -11,3 +13,12 @@ export default {
   name: 'app'
 }
 </script>
+
+<style scoped>
+  .fade-enter-active, .fade-leave-active {
+    transition: opacity .1s;
+  }
+  .fade-enter, .fade-leave-to {
+    opacity: .5;
+  }
+</style>
