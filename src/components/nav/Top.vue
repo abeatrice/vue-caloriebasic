b33e6d079287a4ca749f97322c891d48f9febe4d
abeatrice <abeatrice.mail@gmail.com>
2019-12-17 19:59:57 -0800

    sign out button bg secondary

--- src/components/nav/Top.vue
+++ src/components/nav/Top.vue
@@ -15,13 +15,13 @@
         <div class="text-sm">
             <span id="dropdown" class="flex items-center text-blue-400">
                 <span class="mr-3">{{userName}}</span>
-                <svg @click="open = !open" class="h-4 w-4 fill-current hover:text-teal-100 cursor-pointer" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                <svg @click="open = !open" class="h-4 w-4 fill-current cursor-pointer" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                     <path v-if="!open" d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/>
                     <path v-else d="M10 8.586L2.929 1.515 1.515 2.929 8.586 10l-7.071 7.071 1.414 1.414L10 11.414l7.071 7.071 1.414-1.414L11.414 10l7.071-7.071-1.414-1.414L10 8.586z"/>
                 </svg>
             </span>
-            <div v-show="open" class="absolute bg-white border rounded shadow overflow-hidden">
-                <a @click="signOut" class="no-underline block px-2 py-1 text-gray-800 hover:text-gray-600 cursor-pointer">Sign Out</a>
+            <div v-show="open" class="absolute bg-gray-600 border-none border rounded shadow overflow-hidden">
+                <a @click="signOut" class="no-underline block px-2 py-1 font-bold text-gray-800 cursor-pointer">Sign Out</a>
             </div>
         </div>
     </nav>
